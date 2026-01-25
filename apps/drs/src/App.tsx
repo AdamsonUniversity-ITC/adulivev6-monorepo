adfdf59575f0de2f70ab03daa83852734201aa69
Gabriel Carreon <gabrielcarreon02@gmail.com>
2026-01-26 05:46:25 +0800

    first commit

---
+++ apps/drs/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@repo/ui/components/button";
+import { Card, CardContent } from "@repo/ui/components/card";
+import { Separator } from "@repo/ui/components/separator";
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <Button className="py-20 px-20" variant="outline">TEST</Button>
+    </div>
+  )
+}
+
+export default App;
